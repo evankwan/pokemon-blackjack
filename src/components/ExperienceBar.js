9ccee403625671dd88e8629b4cd94d27afd48379
Jon-Ledo <jonledo.code@gmail.com>
2021-05-25 07:37:29 -0400

    base code for exp-bar animation, based on balance state

--- src/components/ExperienceBar.js
+++ src/components/ExperienceBar.js
@@ -1,8 +1,26 @@
+import {useState} from 'react'
+
 const ExperienceBar = ({ balance }) => {
+
+  const [style, setStyle] = useState({})
+
+
+  setTimeout(() => {
+    const newStyle = {
+      width: `${(balance/10)}%`
+    }
+
+    setStyle(newStyle);
+  }, 100)
+
   return (
     <>
       <div className="expbar">
-        <span className="expbar__title">Player XP: {balance}</span>
+        <div 
+          className="expbar__fill"
+          style={style}
+        ></div>
+        <span className="expbar__title">Player XP: {balance}/1000</span>
       </div>
     </>
   );
