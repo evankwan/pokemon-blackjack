4de8885997169cf97534b949be1e33f09d1df93c
Raid <naji.raid@gmail.com>
2021-05-26 20:39:02 -0400

    added comments

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   const [currentDeck, setCurrentDeck] = useState([]);
   const [gameState, setGameState] = useState(false);
   const [currentBet, setCurrentBet] = useState(0);
-  const [balance, setBalance] = useState(1200);
+  const [balance, setBalance] = useState(1400);
   const [currentPlayer, setCurrentPlayer] = useState('none');
   const [hideButtons, setHideButtons] = useState(false);
   const [error, setError] = useState();
@@ -198,7 +198,7 @@ function App() {
       setPlayAgain(!playAgain);
     }
     if (currentPlayer === 'none') {
-      // reset hand
+      // after the first hand has been played, reset hand
       setHideButtons(true);
       setDealerHand([])
       setPlayerHand([])
@@ -207,7 +207,7 @@ function App() {
   }
 
   useEffect(() => {
-    // logic for player iwnning
+    // logic for player winning a hand
     const playerWinsLogic = async () => {
       // boolean for if player got blackjack
       const blackjack = getScore(playerHand) === 21 && playerHand.length === 2;
@@ -228,8 +228,13 @@ function App() {
       setBalance(newBalance);
       setCurrentMessage(`Player gains ${payout}XP!`);
 
-      // only evolve pokemon if there is another pokemon in the evolution line
+      // pokemonCanEvolve handles the evolution of each pokemon 
+      // each of the pokemon from availablePokemon has an evoluiton line
+      // each pokemon start at the first level of the evolution line. this is represented as the [0] index of its respective array.
+      // each pokemon can only evolve up to the 3rd pokemon in the evolution line
+      // we will only evolve the pokemon if there is another pokemon in the evolution line
       const pokemonCanEvolve = playerPokemon.length > 1
+      console.log(playerPokemon);
       if (pokemonCanEvolve && newBalance >= experienceNeeded) {
 
         await sleep(2000);
@@ -443,6 +448,7 @@ function App() {
                         currentPlayer={currentPlayer}
                       />
                       {/* show double only when player hand is 2 cards */}
+                        {/* the double button is disabled after "hit" or "stand" is clicked */}
                       <ActionBtn
                         name={"Double"}
                         className={"btn btn__double"}
@@ -459,6 +465,7 @@ function App() {
                         hideButtons={hideButtons}
                         currentPlayer={currentPlayer}
                       />
+                      {/* show the deal again button after each hand is over */}
                       <DealAgainButton
                         dealAgain={dealAgain}
                         deckLoaded={currentDeck && currentDeck.length > 0}
@@ -467,12 +474,6 @@ function App() {
 
                     </div>
                   }
-
-
-                  {/* player component holds hand component and sprite component */}
-                  {/* needs hand state */}
-                  {/* <Player /> */}
-
 
                 </div>
                 </div> {/* wrapper div */}
