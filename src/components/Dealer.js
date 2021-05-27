08d18d73e3fd619f96a15a51777e0ae8f1ed9eed
Evan Kwan <evanyuekwan@gmail.com>
2021-05-27 15:25:26 -0400

    fixed indentation on Dealer and GameMessage

--- src/components/Dealer.js
+++ src/components/Dealer.js
@@ -3,22 +3,22 @@ import Sprite from './Sprite';
 
 //this component creates a container to hold the dealer's Sprite (pokemon image) and their hand (hand includes the cards and score)
 const Dealer = ({ hand, dealerPokemon, currentTurn, currentBet }) => {
-    return (
-        <div className="dealer">
-            <div className="dealer__container">
-                <Sprite 
-                pokemon={dealerPokemon} 
-                imgPosition='sprite__dealer'
-                spriteNamePosition='sprite__name-dealer'
-                currentBet={currentBet}/>
+  return (
+    <div className="dealer">
+      <div className="dealer__container">
+        <Sprite 
+        pokemon={dealerPokemon} 
+        imgPosition='sprite__dealer'
+        spriteNamePosition='sprite__name-dealer'
+        currentBet={currentBet}/>
 
-                <Hand 
-                cards={hand} 
-                dealer={true} 
-                currentTurn={currentTurn} />
-            </div>
-        </div>
-    )
+        <Hand 
+        cards={hand} 
+        dealer={true} 
+        currentTurn={currentTurn} />
+      </div>
+    </div>
+  )
 }
 
 export default Dealer;
